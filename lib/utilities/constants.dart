86907bfe9a7da19169d753eba400f8bea5b0c751
Slay2525 <garas43699@gmail.com>
2024-08-16 12:07:44 +0300

    commit

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -20,3 +20,15 @@ const kConditionTextStyle = TextStyle(
 );
 
 const apiKey = 'c06ab1b596795f5e40480fdddf007dd1';
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(Icons.location_city),
+  iconColor: Colors.blue,
+  hintText: 'Enter City Name',
+  border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10.0),
+      ),
+      borderSide: BorderSide.none),
+);
